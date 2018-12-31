31504923ea2b50a57b3e7121507a1175c4fb7994
TeLLie <elbert.pol@gmail.com>
2018-12-31 10:24:41 +0100

    Fix for strnlen

--- daemon/remote/RemoteClient.cpp
+++ daemon/remote/RemoteClient.cpp
@@ -18,7 +18,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-
+#include <string.h> //Fix for strnlen OS2
 #include "nzbget.h"
 #include "RemoteClient.h"
 #include "DownloadInfo.h"
